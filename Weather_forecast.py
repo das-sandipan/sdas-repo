37f507535da4f5923c1431a097a433bd8ab28fc6
das-sandipan <das_sandipan@hotmail.com>
2026-04-03 12:19:57 +0530

    Code to fetch current weather from API

--- Weather_forecast.py
+++ Weather_forecast.py
@@ -7,3 +7,22 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor # for
 from sklearn.metrics import mean_squared_error # to measure accuracy of prediction model
 from datetime import datetime, timedelta # for using date and time
 
+# API KEY Fetching
+API_KEY = '' # put actual API Key
+BASE_URL = ''   # Base URL for making API Request
+
+# Fetch Current Weather Data
+def get_current_weather(city):
+    url = f"{BASE_URL}weather?q={city}&appid={API_KEY}&units=metric" # Construction of API URL
+    response = requests.get(url) # send the get request to API
+    data = response.jason()
+    return {
+        'city': data['name'],
+        'current_temp': round(data['main'] ['temp']),
+        'feels_like': round(data['main'] ['feels_like']),
+        'temp_min': round(data['main'] ['temp_min']),
+        'temp_max': round(data['main'] ['temp_max']),
+        'humidity': round(data['main'] ['humidity']),
+        'description': data['weather'][0]['description'],
+        'country': data['sys']['country'], # 'sys' is given as how API throws data for country code response
+    }
